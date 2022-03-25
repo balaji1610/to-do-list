dfc5e2677c3591cf4eb08ff2bbbbd4d750af76c1
balaji1610 <balajiece1610>
2022-03-25 09:14:28 +0530

    Error Rise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { NEVER } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'to-do-list';
+  /* An empty array that is responsible
+	to add a division */
+  public items = [];
+
+  /* A two-way binding performed which
+	pushes text on division */
+  public newTask: any;
+
+  /* When input is empty, it will
+	not create a new division */
+  public addToList() {
+    if (this.newTask == '') {
+    } else {
+      // this.items.push(this.newTask:NEVER[]);
+      this.newTask = '';
+    }
+  }
+
+  /* This function takes to input the
+	task, that has to be deleted*/
+  public deleteTask(index: any) {
+    this.items.splice(index, 1);
+  }
 }
